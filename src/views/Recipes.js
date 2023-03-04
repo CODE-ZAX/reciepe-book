4a0fff408275340d5291f97b07bb127910462279
Nauman-Javed <naumanj841@gmail.com>
2023-03-04 16:33:12 +0500

    Edited Recipe
    Removed grid

--- src/views/Recipes.js
+++ src/views/Recipes.js
@@ -6,233 +6,209 @@ const Recipes = () => {
     <div className={classes.body}>
       <h1 className="text-center">All Recipes</h1>
       <div className="container text-center">
-        <div className="row">
-          <div className="col">
-            <div className="card mb-3 ">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className={"img-fluid rounded-start" + classes.customImg}
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
-              </div>
+        <div className="card mb-3 ">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className={"img-fluid rounded-start" + classes.customImg}
+                alt="..."
+              />
             </div>
-          </div>
-          <div className="col">
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="card mb-3 ">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className={"img-fluid rounded-start" + classes.customImg}
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
-              </div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
             </div>
-          </div>
-          <div className="col">
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="card mb-3 ">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className={"img-fluid rounded-start" + classes.customImg}
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
               </div>
             </div>
           </div>
-          <div className="col">
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
+        </div>
+        <div className="card mb-3 ">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className={"img-fluid rounded-start" + classes.customImg}
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
               </div>
             </div>
           </div>
-          <div className="col">
-            <div className="card mb-3">
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-                    className="img-fluid rounded-start"
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <h5 className="card-title">Turkey-Pot-Pie</h5>
-                    <p className="card-text">
-                      This Turkey-Pot-Pie is a great alternative to traditional
-                      Pies — especially for pumpkin pie fans!
-                    </p>
-                    <p className="card-text">
-                      <button className="btn btn-primary">BookMark</button>
-                    </p>
-                  </div>
-                </div>
+        </div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card mb-3 ">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className={"img-fluid rounded-start" + classes.customImg}
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="card mb-3">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
+                className="img-fluid rounded-start"
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Turkey-Pot-Pie</h5>
+                <p className="card-text">
+                  This Turkey-Pot-Pie is a great alternative to traditional Pies
+                  — especially for pumpkin pie fans!
+                </p>
+                <p className="card-text">
+                  <button className="btn btn-primary">BookMark</button>
+                </p>
               </div>
             </div>
           </div>
