8649d8c6c162396a005f90e062c9b1f2acd9bdaf
Nauman-Javed <naumanj841@gmail.com>
2023-03-04 08:32:11 +0500

    Created Signup Form
    Did validations
    Error: full name and email validations are not working

--- src/views/Signup.js
+++ src/views/Signup.js
@@ -1,7 +1,170 @@
-import React from "react";
+import React, { useState } from "react";
+import classes from "./Signup.module.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [fullNameEr, setFullNameEr] = useState("");
+  const [emailEr, setEmailEr] = useState("");
+  const [passwordEr, setPasswordEr] = useState("");
+  const [confirmPasswordEr, setConfirmPasswordEr] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    var fullNameE = false,
+      emailE = false,
+      passwordE = false,
+      confirmPasswordE = false;
+
+    if (fullName.length === 0) {
+      fullNameE = true;
+      setFullNameEr("Full Name is Empty");
+    } else if (!fullName.includes(" ")) {
+      fullNameE = true;
+      setFullNameEr("Full Name must have an empty space");
+      console.log("here");
+    } else {
+      fullNameE = false;
+      setFullNameEr("");
+    }
+
+    if (email.length === 0) {
+      emailE = true;
+      setEmailEr("Email is Empty");
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/.test(email)) {
+      emailE = true;
+      setEmailEr("Email is Invalid");
+    } else {
+      emailE = false;
+      setEmailEr("");
+    }
+
+    if (password.length === 0) {
+      passwordE = true;
+      setPasswordEr("Password cannot be Empty");
+    } else if (password.length < 8) {
+      passwordE = true;
+      setPasswordEr("Password must contain atleast 8 characters");
+    } else {
+      passwordE = false;
+      setPasswordEr("");
+    }
+
+    if (confirmPassword.length === 0) {
+      confirmPasswordE = true;
+      setConfirmPasswordEr("Confirm Password cannot be Empty");
+    } else if (password !== confirmPassword) {
+      confirmPasswordE = true;
+      setConfirmPasswordEr("Passwords are not matching");
+    } else {
+      confirmPasswordE = false;
+      setConfirmPasswordEr("");
+    }
+
+    if (!fullNameE && !emailE && !passwordE && !confirmPasswordE) {
+      console.log("Signup");
+    }
+  };
+
+  return (
+    <div className={classes.body}>
+      <div className={classes.signupBody}>
+        <h3 className="text-center mb-3">
+          <strong>Signup</strong>{" "}
+        </h3>
+        <form onSubmit={handleSubmit} noValidate>
+          <div className={"form-floating mb-2"}>
+            <input
+              type="text"
+              onChange={(e) => {
+                setFullName(e.target.value);
+              }}
+              className={
+                fullNameEr.length === 0
+                  ? "form-control"
+                  : "form-control is-invalid"
+              }
+              id="floatingInput"
+              placeholder="John Doe"
+            />
+            <div className="invalid-feedback">{fullNameEr}</div>
+            <label className="floating-label">Full Name</label>
+          </div>
+          <div className={"form-floating mb-2"}>
+            <input
+              type="email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className={
+                emailEr.length === 0
+                  ? "form-control"
+                  : "form-control is-invalid"
+              }
+              id="floatingInput"
+              placeholder="name@example.com"
+            />
+            <div className="invalid-feedback">{emailEr}</div>
+            <label className="floating-label">Email</label>
+          </div>
+          <div className={"form-floating mb-2"}>
+            <input
+              type="password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className={
+                passwordEr.length === 0
+                  ? "form-control"
+                  : "form-control is-invalid"
+              }
+              id="floatingInput"
+              placeholder="*********"
+            />
+            <div className="invalid-feedback">{passwordEr}</div>
+            <label className="floating-label">Password</label>
+          </div>
+          <div className={"form-floating mb-2"}>
+            <input
+              type="password"
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+              }}
+              className={
+                confirmPasswordEr.length === 0
+                  ? "form-control"
+                  : "form-control is-invalid"
+              }
+              id="floatingInput"
+              placeholder="*********"
+            />
+            <div className="invalid-feedback">{confirmPasswordEr}</div>
+            <label className="floating-label">Confirm Password</label>
+          </div>
+          <div className="checkbox mb-3">
+            <label>
+              <input
+                type="checkbox"
+                defaultValue="remember-me"
+                className={classes.chkBox}
+              />{" "}
+              Remember me
+            </label>
+          </div>
+
+          <button
+            className={"w-100 btn btn-primary  " + classes.customBtn}
+            type="submit"
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
