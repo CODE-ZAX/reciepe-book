9d132ea46e9c144c70b9f2e3eeb82eb572869d7b
Nauman-Javed <naumanj841@gmail.com>
2023-03-04 22:17:28 +0500

    Changed RecipeContext and Recipe page
    added details in recipe context

--- src/views/Recipe.js
+++ src/views/Recipe.js
@@ -4,26 +4,23 @@ import classes from "./Recipe.module.css";
 import { BsFillBookmarkStarFill } from "react-icons/bs";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { useRecipe } from "../context/RecipeContext";
+import Error from "./Error";
 
 const Recipe = () => {
   const { rid } = useParams();
   const { recipes } = useRecipe();
   const recipe = recipes[rid];
   const navigate = useNavigate();
-  navigate("gasdgsad");
+
   return (
     <div className={classes.body}>
       <div>
         <div className="card">
-          <img
-            src="https://i.ibb.co/WBw5c0H/Turkey-Pot-Pie.jpg"
-            className="card-img-top"
-            alt="Turkey-Pot-Pie"
-          />
+          <img src={recipe.url} className="card-img-top" alt="Turkey-Pot-Pie" />
           <div className="card-body  d-flex justify-content-between">
             <div>
-              <h5 className="card-title">Turkey Pot Pie</h5>
-              <p className="card-text">Pot-Pie is classic comfort food</p>
+              <h5 className="card-title">{recipe.name}</h5>
+              <p className="card-text">{recipe.details}</p>
             </div>
             <div className="d-flex align-items-center">
               <button className="btn btn-success">
@@ -50,51 +47,36 @@ const Recipe = () => {
           </div>
           <ul className={"list-group list-group-flush " + classes.customList}>
             <li className="list-group-item ">
-              <li>Yield: 1 pie</li>
-              <li>Prep time: 15 minutes</li>
-              <li>Cook time: 10 minutes</li>
-              <li>Total time: 25 minutes</li>
+              <div>
+                <strong>Yield:</strong> {recipe.yield}
+              </div>
+              <div>
+                <strong>Prep Time:</strong> {recipe.preptime}
+              </div>
+              <div>
+                <strong>Cock Time:</strong> {recipe.cocktime}
+              </div>
+              <div>
+                <strong>Total Time:</strong> {recipe.totaltime}
+              </div>
             </li>
-            <h6>Ingredients</h6>
-            <li className="list-group-item">
-              <li>1 cup unsalted butter, softened</li>
-              <li>1 cup white sugar</li>
-              <li>1 cup brown sugar</li>
-              <li>2 large eggs</li>
-              <li>1 tsp vanilla extract</li>
-              <li>3 cups all-purpose flour</li>
-              <li>1 tsp baking soda</li>
-              <li>1 tsp salt</li>
-              <li>2 cups semisweet chocolate chips</li>
-            </li>
-            <h6>Instruction</h6>
-            <li className="list-group-item">
-              <li>Preheat oven to 375°F (190°C).</li>
-              <li>
-                In a large mixing bowl, cream together the butter, white sugar,
-                and brown sugar until light and fluffy.
-              </li>
-              <li>
-                Beat in the eggs one at a time, then stir in the vanilla
-                extract.
-              </li>
-              <li>
-                In a separate bowl, combine the flour, baking soda, and salt.
-                Gradually stir the dry ingredients into the creamed mixture
-                until just blended.
-              </li>
-              <li>Fold in the chocolate chips.</li>
-              <li>
-                Drop tablespoon-sized balls of dough onto ungreased cookie
-                sheets, spacing them about 2 inches apart.
-              </li>
-              <li>Bake for 10-12 minutes, until the edges are golden brown.</li>
-              <li>
-                Remove from oven and allow the cookies to cool on the baking
-                sheet for 5 minutes before transferring them to a wire rack to
-                cool completely.
-              </li>
-            </li>
+            <h6 className="m-3">Ingredients:</h6>
+            <div className="list-group-item">
+              {recipe.ingredients.map((single) => {
+                return (
+                  <li key={single.index}>
+                    <strong>Name: </strong>"{single.name}"
+                    <strong> Quantity: </strong> "{single.quantity}"
+                  </li>
+                );
+              })}
+            </div>
+            <h6 className="m-3">Instructions:</h6>
+            <div className="list-group-item">
+              {recipe.instructions.map((single) => {
+                return <li key={single.index}>{single}</li>;
+              })}
+            </div>
           </ul>
         </div>
       </div>
