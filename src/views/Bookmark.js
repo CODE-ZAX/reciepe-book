8c479976cd10df26d9ce938569d766530308d23a
Nauman-Javed <naumanj841@gmail.com>
2023-03-05 16:26:24 +0500

    Update Bookmark.js
    complete bookmarks

--- src/views/Bookmark.js
+++ src/views/Bookmark.js
@@ -1,7 +1,50 @@
 import React from "react";
+import { useRecipe } from "../context/RecipeContext";
+
+import { BsFillBookmarkPlusFill } from "react-icons/bs";
 
 const Bookmark = () => {
-  return <div>Bookmark</div>;
+  const { recipes } = useRecipe();
+  return (
+    <div style={{ height: "100vh", marginTop: "70px" }}>
+      <h1 className="text-center mb-4">Bookmarks </h1>
+      <div className="container">
+        {recipes.map((recipe) => {
+          return (
+            <div className="card mb-3 " key={recipe.id}>
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img
+                    src={recipe.url}
+                    className={"img-fluid rounded-start"}
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between">
+                      <h5 className="card-title">{recipe.name}</h5>
+                      <div>
+                        <button className="btn btn-success">
+                          <span>
+                            <BsFillBookmarkPlusFill size={20} />
+                          </span>
+                        </button>
+                      </div>
+                    </div>
+                    <p className="card-text ">{recipe.details}</p>
+
+                    <h6>Prepare Time: </h6>
+                    <p>{recipe.preptime}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Bookmark;
