1a7185777f8fbd1d02317fe22dfd36a4a70591e9
Nauman-Javed <naumanj841@gmail.com>
2023-03-05 14:57:27 +0500

    Update Recipes.js
    changed the position of add recipe button

--- src/views/Recipes.js
+++ src/views/Recipes.js
@@ -52,62 +52,21 @@ const Recipes = () => {
   return (
     <div className={classes.body}>
       <h1 className="text-center">All Recipes</h1>
-      <div className="container">
-        {recipes.map((recipe) => {
-          return (
-            <div className="card mb-3 " key={recipe.id}>
-              <div className="row g-0">
-                <div className="col-md-4">
-                  <img
-                    src={recipe.url}
-                    className={"img-fluid rounded-start" + classes.customImg}
-                    alt="..."
-                  />
-                </div>
-                <div className="col-md-8">
-                  <div className="card-body">
-                    <div className="d-flex justify-content-between">
-                      <h5 className="card-title">{recipe.name}</h5>
-                      <div onClick={handleClick}>
-                        {bookmark ? (
-                          <button className="btn btn-success">
-                            <span>
-                              <BsFillBookmarkPlusFill size={20} />
-                            </span>
-                          </button>
-                        ) : (
-                          <button className="btn btn-primary">
-                            <span>
-                              <BsFillBookmarkCheckFill size={20} />
-                            </span>
-                          </button>
-                        )}
-                      </div>
-                    </div>
-                    <p className="card-text ">{recipe.details}</p>
-
-                    <h6>Prepare Time: </h6>
-                    <p>{recipe.preptime}</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-
-        <button
-          onClick={handleAddRecipe}
-          type="button"
-          className="btn btn-primary mt-3 d-flex justify-content-center align-items-center"
-          data-bs-toggle="modal"
-          data-bs-target="#exampleModal"
-        >
-          <span>
-            <IoAddCircleSharp size={25} />
-          </span>
-          Add New Recipe
-        </button>
-
+      <div className="container d-flex justify-content-end">
+        <div className="mb-2">
+          <button
+            onClick={handleAddRecipe}
+            type="button"
+            className="btn btn-primary mt-3 d-flex justify-content-center align-items-center"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+            <span>
+              <IoAddCircleSharp size={25} />
+            </span>
+            Add New Recipe
+          </button>
+        </div>
         <div
           className="modal fade"
           id="exampleModal"
@@ -210,6 +169,50 @@ const Recipes = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="container">
+        {recipes.map((recipe) => {
+          return (
+            <div className="card mb-3 " key={recipe.id}>
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img
+                    src={recipe.url}
+                    className={"img-fluid rounded-start" + classes.customImg}
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between">
+                      <h5 className="card-title">{recipe.name}</h5>
+                      <div onClick={handleClick}>
+                        {bookmark ? (
+                          <button className="btn btn-success">
+                            <span>
+                              <BsFillBookmarkPlusFill size={20} />
+                            </span>
+                          </button>
+                        ) : (
+                          <button className="btn btn-primary">
+                            <span>
+                              <BsFillBookmarkCheckFill size={20} />
+                            </span>
+                          </button>
+                        )}
+                      </div>
+                    </div>
+                    <p className="card-text ">{recipe.details}</p>
+
+                    <h6>Prepare Time: </h6>
+                    <p>{recipe.preptime}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
