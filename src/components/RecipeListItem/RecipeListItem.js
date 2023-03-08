1effd8c030d41eac031f93ce305892b497c00ead
CODE-ZAX <86313946+CODE-ZAX@users.noreply.github.com>
2023-03-08 22:37:41 +0500

    Fixes

--- src/components/RecipeListItem/RecipeListItem.js
+++ src/components/RecipeListItem/RecipeListItem.js
@@ -4,66 +4,104 @@ import { AiFillHeart } from "react-icons/ai";
 import { IoNewspaperOutline } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
 
-const RecipeListItem = ({ recipe, classes, handleFavourite, isEditing }) => {
+const RecipeListItem = ({
+  recipe,
+  classes,
+  handleFavourite,
+  isEditing,
+  handleDelete,
+  setSelected,
+}) => {
   const navigate = useNavigate();
   const recipeId = recipe.id;
   const handleRecipe = () => {
     navigate(`/recipe/${recipeId}`);
   };
 
-  return isEditing ? (
-    <div className="card mb-3 ">
-      <input type="text" />
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img
-            onClick={handleRecipe}
-            src={recipe.url}
-            style={{ cursor: "pointer" }}
-            className={"img-fluid rounded-start" + classes.customImg}
-            alt="..."
-          />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <div className="d-flex justify-content-between">
-              <h5 className="card-title">{recipe.name}</h5>
-              <div>
-                {recipe.favourite ? (
-                  <button
-                    onClick={() => {
-                      handleFavourite(recipe);
-                    }}
-                    className="btn btn-success"
-                  >
-                    <span>
-                      <AiOutlineHeart size={20} />
-                    </span>
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => {
-                      handleFavourite(recipe);
-                    }}
-                    className="btn btn-primary"
-                  >
-                    <span>
-                      <AiFillHeart size={20} />
-                    </span>
-                  </button>
-                )}
+  return (
+    <div>
+      <div className="d-flex align-items-center">
+        {isEditing && (
+          <div className="me-2">
+            <input
+              type="checkbox"
+              onChange={(e) => {
+                if (e.target.checked) {
+                  setSelected((prev) => [...prev, recipe]);
+                } else {
+                  setSelected((prev) =>
+                    prev.filter((item) => {
+                      return item.id !== recipe.id;
+                    })
+                  );
+                }
+              }}
+            />
+          </div>
+        )}
+        <div className="card mb-3 w-100 ">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img
+                onClick={handleRecipe}
+                src={recipe.url}
+                style={{ cursor: "pointer" }}
+                className={"img-fluid rounded-start" + classes.customImg}
+                alt="..."
+              />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <div className="d-flex justify-content-between">
+                  <h5 className="card-title">{recipe.name}</h5>
+                  <div>
+                    {recipe.favourite ? (
+                      <button
+                        onClick={() => {
+                          handleFavourite(recipe);
+                        }}
+                        className="btn btn-success"
+                      >
+                        <span>
+                          <AiOutlineHeart size={20} />
+                        </span>
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => {
+                          handleFavourite(recipe);
+                        }}
+                        className="btn btn-primary"
+                      >
+                        <span>
+                          <AiFillHeart size={20} />
+                        </span>
+                      </button>
+                    )}
+                  </div>
+                </div>
+                <p className="card-text ">{recipe.details}</p>
+
+                <h6>Prepare Time: </h6>
+                <p>{recipe.preptime}</p>
               </div>
             </div>
-            <p className="card-text ">{recipe.details}</p>
-
-            <h6>Prepare Time: </h6>
-            <p>{recipe.preptime}</p>
           </div>
         </div>
+        {isEditing && (
+          <div className="ms-2">
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                handleDelete(recipe);
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        )}
       </div>
     </div>
-  ) : (
-    <div>Hello</div>
   );
 };
 
--- src/views/Recipes.js
+++ src/views/Recipes.js
@@ -4,120 +4,26 @@ import { useRecipe } from "../context/RecipeContext";
 import { IoAddCircleSharp } from "react-icons/io5";
 import RecipeListItem from "../components/RecipeListItem/RecipeListItem";
 import { AiFillEdit } from "react-icons/ai";
+import MainForm from "../components/Forms/MainForm";
 
 const Recipes = () => {
   // const [bookmark, setBookmark] = useState(true);
 
   const [isEditing, setIsEditing] = useState(false);
-  const [formData, setFormData] = useState({
-    favourite: false,
-    id: Math.floor(Math.random() * 100000),
-    url: "",
-    name: "",
-    details: "",
-    yield: "",
-    preptime: "",
-    cooktime: "",
-    totaltime: "",
-    ingredients: [],
-    instructions: [],
-  });
-  const [showIngredientForm, setShowIngredientForm] = useState(false);
-  const [showInstructionsForm, setShowInstructionsForm] = useState(false);
-  const { recipes, handleNewRecipe, handleFavourite } = useRecipe();
-  console.log(recipes);
-  // const handleClick = () => {
-  //   setBookmark(!bookmark);
-  // };
-  const submitForm = (e) => {
-    handleNewRecipe(formData);
+  const [selected, setSelected] = useState([]);
+  const { recipes, handleFavourite, handleDelete } = useRecipe();
+
+  const delMul = () => {
+    selected.forEach((item) => {
+      handleDelete(item.id);
+    });
   };
 
   const handleEdit = () => {
     setIsEditing(!isEditing);
-    console.log("Here");
+    setSelected([]);
   };
-
-  const AddIngredient = ({ setFormData, setShowIngredientForm }) => {
-    const [ingredient, setIngredient] = useState("");
-    const [quantity, setQuantity] = useState(0);
-    const onFormSubmit = () => {
-      if (ingredient.length !== 0 && quantity !== 0) {
-        setFormData({
-          ...formData,
-          ingredients: [
-            ...formData.ingredients,
-            { name: ingredient, quantity: quantity },
-          ],
-        });
-      }
-      setShowIngredientForm(false);
-    };
-    return (
-      <div>
-        <div className="row justify-content-between g-0">
-          <div className="col-8  mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="floatingPassword"
-              placeholder={`Ingredient ${formData.ingredients.length + 1}`}
-              onChange={(e) => {
-                setIngredient(e.target.value);
-              }}
-            />
-          </div>
-          <div className="col-3  mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="floatingPassword"
-              placeholder="Qty"
-              onChange={(e) => {
-                setQuantity(e.target.value);
-              }}
-            />
-          </div>
-        </div>
-        <div onClick={onFormSubmit} className="btn btn-primary">
-          Save
-        </div>
-      </div>
-    );
-  };
-
-  const AddInstructions = ({ setFormData, setShowInstructionsForm }) => {
-    const [instruction, setInstruction] = useState("");
-    const onFormSubmit = () => {
-      if (instruction.length !== 0) {
-        setFormData({
-          ...formData,
-          instructions: [...formData.instructions, instruction],
-        });
-      }
-      setShowInstructionsForm(false);
-    };
-    return (
-      <div>
-        <div className="row justify-content-between g-0">
-          <div className="col-8  mb-3">
-            <input
-              type="text"
-              className="form-control"
-              id="floatingPassword"
-              placeholder={`Instruction ${formData.instructions.length + 1}`}
-              onChange={(e) => {
-                setInstruction(e.target.value);
-              }}
-            />
-          </div>
-        </div>
-        <div onClick={onFormSubmit} className="btn btn-primary">
-          Save
-        </div>
-      </div>
-    );
-  };
+  console.log(selected);
   return (
     <div className={classes.body}>
       <h1 className="text-center">All Recipes</h1>
@@ -145,191 +51,16 @@ const Recipes = () => {
             Add New Recipe
           </button>
         </div>
-        <div
-          className="modal fade"
-          id="exampleModal"
-          tabindex="-1"
-          aria-labelledby="exampleModalLabel"
-          aria-hidden="true"
-        >
-          <div className="modal-dialog">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title" id="exampleModalLabel">
-                  Add Recipe
-                </h5>
-                <button
-                  type="button"
-                  className="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
-              </div>
-              <div className="modal-body">
-                <div>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingInput"
-                      placeholder="https://abc.com"
-                      onChange={(e) => {
-                        setFormData({ ...formData, url: e.target.value });
-                      }}
-                    />
-                    <label for="floatingInput">Image Url</label>
-                  </div>
-                  <div className="form-floating mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingInput"
-                      placeholder="name@example.com"
-                      onChange={(e) => {
-                        setFormData({ ...formData, name: e.target.value });
-                      }}
-                    />
-                    <label for="floatingInput">Recipe Name</label>
-                  </div>
-                  <div className="form-floating  mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setFormData({ ...formData, details: e.target.value });
-                      }}
-                    />
-                    <label for="floatingPassword">Description</label>
-                  </div>
-                  <div className="form-floating  mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setFormData({ ...formData, yield: e.target.value });
-                      }}
-                    />
-                    <label for="floatingPassword">Yield</label>
-                  </div>
-                  <div className="form-floating  mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setFormData({ ...formData, preptime: e.target.value });
-                      }}
-                    />
-                    <label for="floatingPassword">Prep Time</label>
-                  </div>
-                  <div className="form-floating  mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setFormData({ ...formData, cooktime: e.target.value });
-                      }}
-                    />
-                    <label for="floatingPassword">Cook Time</label>
-                  </div>
-                  <div className="form-floating  mb-3">
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="floatingPassword"
-                      placeholder="Password"
-                      onChange={(e) => {
-                        setFormData({ ...formData, totaltime: e.target.value });
-                      }}
-                    />
-                    <label for="floatingPassword">Total Time</label>
-                  </div>
-                  <div className=" mb-3">
-                    <label>Ingredients</label>
-                    <ul>
-                      {formData.ingredients.map((ingredient, index) => (
-                        <li key={index}>
-                          <strong>Name:</strong> <span>{ingredient.name}</span>
-                          <strong>Quantity:</strong>{" "}
-                          <span>{ingredient.quantity}</span>
-                        </li>
-                      ))}
-                    </ul>
-                    {showIngredientForm ? (
-                      <AddIngredient
-                        setFormData={setFormData}
-                        setShowIngredientForm={setShowIngredientForm}
-                      />
-                    ) : (
-                      <button
-                        onClick={() => {
-                          setShowIngredientForm(true);
-                        }}
-                        className={"btn btn-primary"}
-                      >
-                        Add Ingredient
-                      </button>
-                    )}
-                  </div>
-                  <div className=" mb-3">
-                    <label>Instructions</label>
-                    <ul>
-                      {formData.instructions.map((instruction, index) => (
-                        <li key={index}>{instruction}</li>
-                      ))}
-                    </ul>
-                    {showInstructionsForm ? (
-                      <AddInstructions
-                        setFormData={setFormData}
-                        setShowInstructionsForm={setShowInstructionsForm}
-                      />
-                    ) : (
-                      <button
-                        onClick={() => {
-                          setShowInstructionsForm(true);
-                        }}
-                        className={"btn btn-primary"}
-                      >
-                        Add Ingredient
-                      </button>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button
-                  type="button"
-                  className="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
-                  Close
-                </button>
-                <div
-                  onClick={submitForm}
-                  className="btn btn-primary"
-                  data-bs-dismiss="modal"
-                >
-                  Add
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <MainForm />
       </div>
-
       <div className="container">
         {recipes.map((recipe) => {
           return (
             <RecipeListItem
+              setSelected={setSelected}
               recipe={recipe}
               classes={classes}
+              handleDelete={handleDelete}
               handleFavourite={handleFavourite}
               bookmark={true}
               isEditing={isEditing}
